8bdafad23f21897f7095c30201a6116f9901cd09
Nayanne Batista <nayanne.lb@gmail.com>
2021-03-25 22:19:06 -0300

    implemented class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ export default props => {
 
     return (
         <button 
-            onClick={e => props.click(props.label)}
+            onClick={e => props.click && props.click(props.label)}
             className={classes}>
             {props.label}
         </button>
